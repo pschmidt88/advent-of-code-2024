20831f2ec59ba3eb004c1b0eb0128ce29c2b14cc
Paul Schmidt <rookian@gmail.com>
2024-12-03 22:26:36 +0100

    Finished day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,46 @@
+import java.util.regex.Pattern
+import kotlin.math.absoluteValue
+
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = splitInputToPairOfLists(input).let {
+            it.first.sorted() to it.second.sorted()
+        }
+
+        return left.mapIndexed { index, value ->
+            (value - right[index]).absoluteValue
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val (left, right) = splitInputToPairOfLists(input).let {
+            it.first.sorted() to it.second.sorted()
+        }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        return left.sumOf { number ->
+            number * right.count { it == number }
+        }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun splitInputToPairOfLists(input: List<String>): Pair<List<Int>, List<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    for (line in input) {
+        val (first, second) = line.split(Pattern.compile("\\s+"))
+        left.add(first.toInt())
+        right.add(second.toInt())
+    }
+    return left to right
 }
